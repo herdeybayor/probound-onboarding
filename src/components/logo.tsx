6c0a2ed1dcde3677157fe2acfd309c999644333b
herdeybayor <shereefdeen4real@gmail.com>
2025-06-23 22:42:47 +0100

    feat: add logo component and logo image

---
+++ src/components/logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export interface LogoProps {
+    containerClassName?: string;
+    className?: string;
+    href?: string;
+    width?: number;
+    height?: number;
+}
+
+export default function Logo({ containerClassName, className, href = "/", width = 154, height = 43 }: LogoProps) {
+    return (
+        <Link href={href} className={containerClassName}>
+            <Image src="/images/logo.png" alt="Probound" width={width} height={height} className={className} />
+        </Link>
+    );
+}
